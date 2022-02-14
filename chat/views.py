cdcc58b437dedc9c10fbacd3e3b64af4e99c85f5
unknown <aungmyomin92263@gmail.com>
2022-02-14 21:58:32 +0630

    first commit

---
+++ chat/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.decorators import login_required
+import json
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'chat/index.html')
+
+
+# @login_required
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username))
+    })
